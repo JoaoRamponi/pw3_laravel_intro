24211bcfc9d27aca767fe8550e86af2cb86f7a32
JoaoRamponi <joaoramponi6@gmail.com>
2026-06-10 16:44:20 -0300

    webFeito

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\OficinaController;
+use App\Http\Controllers\livrosController;
+use App\Http\Controllers\PrudutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/oficinas', [OficinaController::class, 'index']);
 Route::post('/oficinas', [OficinaController::class, 'store']);
+
+Route::get('/produtos', [PrudutoController::class, 'index']);
+Route::post('/produtos', [PrudutoController::class, 'store']);
+
+Route::get('/livros', [livrosController::class, 'index']);
+Route::post('/livros', [livrosController::class, 'store']);
